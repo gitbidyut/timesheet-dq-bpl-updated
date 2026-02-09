364d14032734f3a4d75c502965423470a6bf377f
Bidyut Pal <bidyut.pal@atos.net>
2026-02-09 15:08:52 +0530

    all files

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,3 +10,9 @@ resource "aws_s3_bucket" "artifacts" {
   bucket        = "bpl-timesheet-artifacts-${var.env}"
   force_destroy = true
 }
+
+resource "aws_s3_object" "my_directory" {
+  bucket = aws_s3_bucket.raw.id
+  key    = "input/" # The trailing slash indicates a directory
+  # No source or content is needed for an empty directory.
+}
